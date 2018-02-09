3ae6f9813d4b2b80e62bff7c5f75433c4400b1c5
jdumont0201 <32376919+jdumont0201@users.noreply.github.com>
2018-02-09 15:14:36 +0100

    add welcome dedicated page

--- src/pages/welcome/component.ts
+++ src/pages/welcome/component.ts
@@ -7,6 +7,8 @@ import {PageWithTabs} from "../../lib/localton/components/PageWithTabs/component
 import {RefreshService} from "../../lib/localton/services/refresh.service";
 import {EventService} from "../../lib/localton/services/event.service";
 import {ConsoleService} from "../../lib/globalton/core/services/console.service";
+import {AuthService} from "../../lib/globalton/core/services/auth.service";
+import {Router} from "@angular/router";
 @Component({
   selector: 'app-page-welcome',
   templateUrl:'template.html'
@@ -15,9 +17,13 @@ import {ConsoleService} from "../../lib/globalton/core/services/console.service"
 @Injectable()
 export class AppWelcomePage extends PageWithTabs {
 
-  constructor(public requestService: RequestService, public consoleService:ConsoleService  , public dataService:DataService,public refreshService:RefreshService,public eventService:EventService) {
+  constructor(public authService:AuthService,public router:Router,public requestService: RequestService, public consoleService:ConsoleService  , public dataService:DataService,public refreshService:RefreshService,public eventService:EventService) {
       super(refreshService,eventService,consoleService)
-
+      console.log("redirect welc")
+        if(this.authService.isAuthenticated()){
+          console.log("redirect")
+            this.router.navigate(["/dashboard/today"]);
+        }
   }
 
 
